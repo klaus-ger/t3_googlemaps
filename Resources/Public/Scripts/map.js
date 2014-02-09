12299163c9067872e61a87e2fdfcf3b5f5f0ae48
Klaus Heuer <post@heuer-illustration.de>
2014-02-09 16:44:03 +0100

    changed Map settings from display country names to city names add function render MainRegions

--- Resources/Public/Scripts/map.js
+++ Resources/Public/Scripts/map.js
@@ -10,11 +10,23 @@ var styles = [{
     }
     ]
 },{
-    "featureType": "administrative.country",
+    "featureType": "administrative.city",
     "elementType": "labels.text.fill",
     "stylers": [
     {
         "visibility": "on"
+    },
+    {
+        "color": "#000000"
+    }
+    
+    ]
+},{
+    "featureType": "administrative.country",
+    "elementType": "labels.text.fill",
+    "stylers": [
+    {
+        "visibility": "off"
     },
     {
         "color": "#000000"
@@ -42,11 +54,10 @@ function initialize() {
 
     map = new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
     
-    renderRegions();
+    renderMainRegions();
+    //renderRegions();
 
 }
-
-
 
 
 function loadScript() {
@@ -55,9 +66,6 @@ function loadScript() {
     script.src = "http:///maps.googleapis.com/maps/api/js?libraries=geometry&sensor=false&callback=initialize";
     document.body.appendChild(script);
 }
-
-
-       
 
 
 window.onload = loadScript;
