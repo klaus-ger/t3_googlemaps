aa20de04902a738dc38af571039def74ce42462b
Klaus Heuer <post@heuer-illustration.de>
2014-02-10 00:33:03 +0100

    Add zoom level changed function

--- Resources/Public/Scripts/map.js
+++ Resources/Public/Scripts/map.js
@@ -1,39 +1,39 @@
 var map;
 
 var styles = [{
-    "stylers": [
-    {
-        "visibility": "on"
-    },
-    {
-        "color": "#ffffff"
-    }
-    ]
-},{
-    "featureType": "administrative.city",
-    "elementType": "labels.text.fill",
-    "stylers": [
-    {
-        "visibility": "on"
-    },
-    {
-        "color": "#000000"
-    }
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },{
+        "featureType": "administrative.city",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#000000"
+            }
     
-    ]
-},{
-    "featureType": "administrative.country",
-    "elementType": "labels.text.fill",
-    "stylers": [
-    {
-        "visibility": "off"
-    },
-    {
-        "color": "#000000"
-    }
+        ]
+    },{
+        "featureType": "administrative.country",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "visibility": "off"
+            },
+            {
+                "color": "#000000"
+            }
     
-    ]
-}
+        ]
+    }
 ];
 
 function initialize() {
@@ -55,18 +55,50 @@ function initialize() {
     map = new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
     
     renderMainRegions();
-    //renderRegions();
+    renderRegions();
+    
+    //******************************************************************************
+    // Change Zoom Level Function
+    //******************************************************************************
+    google.maps.event.addListener(map, 'zoom_changed', function() {
+        zoomLevel = map.getZoom();
+        
+        // ZOOM LEVEL 6: show one digit PLZ area ********************************
+        if (zoomLevel == 6) {
+            //remove two digit polys
+            for (i = 0; i<countrypolys.length; i++){
+                countrypolys[i].setVisible(false);
+            }
+            //set one digti polys
+            for (i = 0; i<mainregionpolys.length; i++){
+                mainregionpolys[i].setVisible(true);
+            }
+        }
+        
+        // ZOOM LEVEL 7: show two digit PLZ area ********************************
+        if (zoomLevel == 7) {
+            //remove two digit polys
+            for (i = 0; i<countrypolys.length; i++){
+                countrypolys[i].setVisible(true);
+            }
+            //set one digti polys
+            for (i = 0; i<mainregionpolys.length; i++){
+                mainregionpolys[i].setVisible(false);
+            }
+        }
 
-}
+    });
+    
+};
 
 
-function loadScript() {
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = "http:///maps.googleapis.com/maps/api/js?libraries=geometry&sensor=false&callback=initialize";
-    document.body.appendChild(script);
-}
+    function loadScript() {
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = "http:///maps.googleapis.com/maps/api/js?libraries=geometry&sensor=false&callback=initialize";
+        document.body.appendChild(script);
+    }
 
 
-window.onload = loadScript;
+    window.onload = loadScript;
 
